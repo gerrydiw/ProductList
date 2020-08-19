1462f553cca5f03a178d126980b1856deb5b32b0
gerrydiw <gerrydiw123@gmail.com>
2020-08-19 20:21:58 +0700

    checkpoint 5

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,7 +23,7 @@ class Header extends Component {
   render() {
     return (
       <View style={ style.container }>
-        <Text style={ style.text }>Header</Text>
+        <Text style={ style.text }>{this.props.title}</Text>
       </View>
     );
   }
